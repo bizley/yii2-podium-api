df93d9bff271e31b72dc8e5cbd8495c1e34c10e0
Paweł Brzozowski <pawel@positive.codes>
2018-07-24 19:50:58 +0200

    enums to move

--- to-be-moved/podium.enum.php
+++ to-be-moved/podium.enum.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    'acquaintance.type.friend' => 'Friend',
-    'acquaintance.type.ignore' => 'Ignore',
-    'member.status.active' => 'Active',
-    'member.status.banned' => 'Banned',
-    'member.status.registered' => 'Registered',
     'setting.maintenance.mode' => 'Maintenance Mode',
     'setting.members.visible' => 'Visible Members',
     'setting.merge.posts' => 'Merge Posts On Appending',
